690a6c09d6bdf9f3a06ae05fdf5d0670fbaef95a
muzammil <muzammilabid08@gmail.com>
2026-08-11 04:20:59 +0500

    Update utils.js

--- assests/js/utils.js
+++ assests/js/utils.js
@@ -1,25 +1,22 @@
-import { getEmployees, getCurrentUser } from "./storage.js";
-const allEmployees = getEmployees();
-const currCompanyId = getCurrentUser();
-const currCompanyEmployees = allEmployees.filter(employee => employee.companyId === currCompanyId);
 export const generatedId = () => {
     const date = Date.now().toString().slice(-6);
     const random = Math.floor(100 + Math.random() * 900);
-    return `EMP-${date}${random}`
-}
+
+    return `EMP-${date}${random}`;
+};
 
 export const value = (inp) => {
     return inp.value.toLowerCase().trim().replaceAll(/\s+/g, " ");
-}
+};
 
 export const getElemBySelector = (selector) => {
     return document.querySelector(selector);
-}
+};
 
-export const search = (inp) => {
+export const search = (inp, employees) => {
     const query = inp.trim().toLowerCase();
 
-    return currCompanyEmployees.filter(employee =>
+    return employees.filter(employee =>
         employee.firstName.toLowerCase().includes(query) ||
         employee.lastName.toLowerCase().includes(query) ||
         employee.email.toLowerCase().includes(query) ||
