22c79050ca1dfaa146877bf9a3781c095dd1a039
muzammil <muzammilabid08@gmail.com>
2026-08-11 04:12:51 +0500

    Update signup.js

--- assests/js/signup.js
+++ assests/js/signup.js
@@ -1,5 +1,5 @@
 import { value } from "./utils.js";
-import { saveCompanies, getCompanies, saveCurrentUser } from "./storage.js";
+alert("UTILS WORKS");
 
 
 const showPassEye = document.querySelector(".password-toggle");
